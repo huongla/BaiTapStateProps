55bcaa1e09ca81359e9ad51d5097546ca6074632
HuongLa <huongla>
2020-11-18 18:22:43 +0700

    BaiTapStateProps

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BaiTapStateProps from './BaiTapStateProps/BaiTapStateProps';
+
+function App() {
+  return (
+    <div className="App">
+    <BaiTapStateProps/>
+    </div>
+  );
+}
+
+export default App;
